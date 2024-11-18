c7844409d0feb274c560a0048a22e93302028f58
eslamehab2022 <eslamehab991@gmail.com>
2024-11-18 13:56:28 +0200

    create auth model

---
+++ src/modules/Auth/controller/auth.controller.js
@@ -0,0 +1,17 @@
+import { asyncHandelr } from "../../utils/error.js";
+
+
+
+export const signUp = asyncHandelr(async(req,res,next)=>{
+
+});
+
+
+
+export const login = asyncHandelr(async(req,res,next)=>{
+
+});
+export const forgetPassword = asyncHandelr(async(req,res,next)=>{
+
+});
+
--- src/modules/Auth/routes/auth.routes.js
+++ src/modules/Auth/routes/auth.routes.js
@@ -1,11 +1,15 @@
 import { Router } from "express";
-
+import * as authController from "../controller/auth.controller.js"
 const authRouter = Router();
 
+//sign up user
+authRouter.post('/signup',authController.signUp);
 
+//sign in user
+authRouter.post('/signup',authController.login);
 
-
-
+// forget password used for login
+authRouter.post('/forgetpassword', authController.forgetPassword)
 
 
 export default authRouter
