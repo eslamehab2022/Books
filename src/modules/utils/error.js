ce41d3207242228536c724ed8e25e3668ae985e3
eslamehab2022 <eslamehab991@gmail.com>
2024-11-22 16:26:09 +0200

    create mail temp

--- src/modules/utils/error.js
+++ src/modules/utils/error.js
@@ -17,3 +17,4 @@ export const errorGlobal =(err,req,res,next)=>{
     }
 
 }
+
---
+++ src/modules/utils/mail.js
@@ -0,0 +1,71 @@
+
+import dotenv from "dotenv";
+dotenv.config({});
+import  sgMail from '@sendgrid/mail'
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+
+
+export const sendMail = async ({ to, subject, html = "", text = "" } = {}) => {
+  try {
+    const msg = {
+      to, 
+      from: process.env.EMAIL_APP, 
+      subject, 
+      text, 
+      html,
+    };
+
+    const response = await sgMail.send(msg);
+    console.log(`Email sent successfully! Status Code: ${response[0].statusCode}`);
+    return { success: true, statusCode: response[0].statusCode };
+  } catch (error) {
+    console.error(`Failed to send email: ${error.message}`);
+    return { success: false, error: error.message };
+  }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export const sendMail =({to, subject ,html , text}={})=>{
+
+//     const msg = {
+//         to: to, 
+//         from: process.env.EMAIL_APP, 
+//         subject:subject,
+//         text: text,
+//         html: html,
+//       }
+      
+//       sgMail
+//         .send(msg)
+//         .then((response) => {
+//           console.log(response[0].statusCode)
+//           console.log(response[0].headers)
+//         })
+//         .catch((error) => {
+//           console.error(error)
+//         })
+
+//         return msg;
+//       }
