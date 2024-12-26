bed03ddb74a57ef08ab74ced847ba30314b76564
eslamehab2022 <eslamehab991@gmail.com>
2024-12-26 13:53:20 +0200

    last updates

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import authRouter from "./modules/Auth/routes/auth.routes.js";
 import categoryRouter from "./modules/category/routes/category.routes.js";
+import taskRouter from "./modules/tasks/routes/tasks.routes.js";
 import userRouter from "./modules/users/routes/users.routes.js";
 import { errorGlobal } from "./modules/utils/error.js";
 import connectionDb from "./server/connection.js";
@@ -14,7 +15,9 @@ const initApp =(app,express)=>{
  connectionDb();
  app.use("/api/v1/Auth",authRouter)
  app.use("/api/v1/user",userRouter);
- app.use("/api/v1/category",categoryRouter)
+ app.use("/api/v1/category",categoryRouter);
+ app.use("/api/v1/tasks",taskRouter)
+
 
  app.use(errorGlobal);
 }
